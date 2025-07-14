2a551d901e4afdf959e44659f7ea74c8dc02257a
Guru Jaswanth <8254964@ups.com>
2025-07-14 22:28:07 +0530

    Added meeting room details and new tiles

---
+++ UpsBookingApp/UpsBookingApp/Migrations/20250714162535_AddDescriptionToDesk.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UpsBookingApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionToDesk : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Desks",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Desks");
+        }
+    }
+}
