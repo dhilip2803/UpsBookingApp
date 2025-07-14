6edca9fbd40ae522238da07b368a848b66f060a0
Sivagurunathan S <8240087@ups.com>
2025-07-14 23:01:27 +0530

    prod depoly

--- UpsBookingApp/UpsBookingApp/Program.cs
+++ UpsBookingApp/UpsBookingApp/Program.cs
@@ -27,6 +27,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
     DbSeeder.Seed(db);
 }
 // Configure the HTTP request pipeline.
